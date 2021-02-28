e985c92bdb642ac493a0be311e8c4a3f01e44218
Cristian Mocanu <cvmocanu@gmail.com>
2021-02-28 20:22:36 +0100

    add "testName" and "testPath" to "testContext"

--- testerum-steps-api/src/main/kotlin/com/testerum_api/testerum_steps_api/test_context/TestContext.kt
+++ testerum-steps-api/src/main/kotlin/com/testerum_api/testerum_steps_api/test_context/TestContext.kt
@@ -4,7 +4,11 @@ import com.testerum_api.testerum_steps_api.services.TesterumService
 
 interface TestContext : TesterumService {
 
-    val testStatus: ExecutionStatus
     val stepsClassLoader: ClassLoader
+
+    val testName: String?
+    val testPath: String?
+
+    val testStatus: ExecutionStatus?
 
 }
