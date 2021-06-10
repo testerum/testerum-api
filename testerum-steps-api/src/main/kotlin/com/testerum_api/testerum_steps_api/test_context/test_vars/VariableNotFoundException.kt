ac520cab18bccdac06132c198feb6bbd938d69b5
Cristian Mocanu <cvmocanu@gmail.com>
2021-06-11 00:00:12 +0200

    small fixes

--- testerum-steps-api/src/main/kotlin/com/testerum_api/testerum_steps_api/test_context/test_vars/VariableNotFoundException.kt
+++ testerum-steps-api/src/main/kotlin/com/testerum_api/testerum_steps_api/test_context/test_vars/VariableNotFoundException.kt
@@ -1,3 +1,5 @@
 package com.testerum_api.testerum_steps_api.test_context.test_vars
 
-class VariableNotFoundException(val varName: String) : RuntimeException("cannot find variable [$varName]")
+class VariableNotFoundException(
+    @Suppress("MemberVisibilityCanBePrivate") val varName: String
+) : RuntimeException("cannot find variable [$varName]")
